04aa72226ee36275765c89d66aff3b5dc6357bf3
natuchasca <natuxalee@gmail.com>
2016-06-06 13:39:07 -0400

    js

---
+++ js/ejercicio.js
@@ -0,0 +1,26 @@
+// METROS
+/*
+var metros;
+var pulgadas;
+metros = prompt("Por avor ingresa los metros", "")
+pulgadas = metros / 0.0245;
+alert("las pulgadas son: "+ pulgadas);
+
+
+// O de esta manera con un function o caja de js
+function metros_a_pulgadas (metros) {
+	var pulgadas;
+	pulgadas = metros / 0.0245;
+	return pulgadas;
+}
+
+var metros = prompt("Por avor ingresa los metros", "");
+var pulgadas = metros_a_pulgadas (metros);
+alert("las pulgadas son: "+ pulgadas);
+
+
+*/
+
+
+//SUELDO
+
